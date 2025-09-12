5579f742b265be6ce6889fb3e77935b88b5cf81e
Jaskaran <thecodersingh@gmail.com>
2025-09-11 21:52:24 -0700

    Update ESLint plugin to @nabla/vite-plugin-eslint in package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import eslint from 'vite-plugin-eslint';
+import eslintPlugin from '@nabla/vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
