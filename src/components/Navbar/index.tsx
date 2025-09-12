da7ff939a2f38ae1bd4af64dfd4adcf7886d790a
Jaskaran <thecodersingh@gmail.com>
2025-09-11 18:30:26 -0700

    Add Iconoir icons and implement responsive Navbar, update styles and constants

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,41 @@
+import { useState } from 'react';
+import { NAVBAR } from '../../constants/navbar';
+import { Menu } from 'iconoir-react';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav>
+      {/* Mobile */}
+      <div className="md:hidden flex justify-between items-center p-4">
+        <div>
+          <a href="/">Logo</a>
+        </div>
+        <div>
+          <button onClick={() => setIsOpen(!isOpen)}>
+            <Menu />
+          </button>
+        </div>
+      </div>
+      <div
+        className={`md:hidden ${isOpen ? 'block' : 'hidden'} py-4 fixed bg-gradient-main w-full`}
+      >
+        <ul>
+          {NAVBAR.links.map((link) => (
+            <li key={link.label} className="py-4 text-center">
+              <a href={link.href} className="uppercase">
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Desktop */}
+      <div></div>
+    </nav>
+  );
 };
 
 export default Navbar;
---
+++ src/constants/navbar.ts
@@ -0,0 +1,9 @@
+export const NAVBAR = {
+  links: [
+    { label: 'About', href: '#about' },
+    { label: 'Schedule', href: '#schedule' },
+    { label: 'Event', href: '#event' },
+    { label: 'Tickets', href: '#volunteer' },
+    { label: 'Sponsor', href: '#sponsor' },
+  ],
+};
